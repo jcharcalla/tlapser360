757816bcde059ca8f82284a6707e2cae75d6d833
jcharcalla <jcharcalla@gmail.com>
2016-10-21 19:14:35 -0500

    Fixed wrong hex value for real this time

--- shutterspeed_to_hex.sh
+++ shutterspeed_to_hex.sh
@@ -26,8 +26,8 @@ ss_convert () {
      else
 	  echo "Greater then 1"
           # If its greater than one but not a decimal or something like this 30/1
-          SSPEED_SCALED=$(echo "scale=0; ${SSPEED}*10/1" | bc -l)
-          SSPEED_HEX1=$(printf "%08x\n" ${SSPEED_SCALED} | awk '{print substr ($0,7,2) substr ($0,5,2) substr ($0,3,2) substr ($0,1,2)}' | sed 's/.\{2\}/&\\x/g' | sed -e 's/^/\\x/' | awk '{print substr($0,1,length()-2)}')
+          #SSPEED_SCALED=$(echo "scale=0; ${SSPEED}*10/1" | bc -l)
+          SSPEED_HEX1=$(printf "%08x\n" ${SSPEED} | awk '{print substr ($0,7,2) substr ($0,5,2) substr ($0,3,2) substr ($0,1,2)}' | sed 's/.\{2\}/&\\x/g' | sed -e 's/^/\\x/' | awk '{print substr($0,1,length()-2)}')
           # The 2nd hex intiger in this case is always a 1, or a
           SSPEED_HEX2="\x01\x00\x00\x00"
      fi
